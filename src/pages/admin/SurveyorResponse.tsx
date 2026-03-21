75d67d63b698cf00aadced98a00ec005f89180bc
siddharth <project@fathommarineconsultants.com>
2026-03-21 13:39:55 +0530

    url fix

--- src/pages/admin/SurveyorResponse.tsx
+++ src/pages/admin/SurveyorResponse.tsx
@@ -2,7 +2,7 @@
         import { useParams } from "react-router-dom";
         import { toast } from "sonner";
 
-        const BASE_URL = "http://localhost:5000"; // Apne backend URL se replace karein
+        const BASE_URL = "https://fmc-client-admin-dashboard-backend.vercel.app"; // Apne backend URL se replace karein
 
         const SurveyorResponse = () => {
         const { token } = useParams();
